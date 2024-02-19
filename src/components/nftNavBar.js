00fd5f58b14057a5597e63815503fc7350e497ea
Liam <liamerickson20@gmail.com>
2024-02-19 05:20:28 -0800

    Add NftNavBar component

---
+++ src/components/nftNavBar.js
@@ -0,0 +1,35 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import React from 'react';
+
+const NftNavBar = () => {
+    return (
+        <div id='nft-nav-bar'>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <input className='search' />
+        </div>
+    )
+}
+
+export default NftNavBar;
