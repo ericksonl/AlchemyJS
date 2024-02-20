93ac61202ecfdd61ab73a388c77a441b8cdcdd3d
Liam <liamerickson20@gmail.com>
2024-02-19 16:30:37 -0800

    Refactor NFTWallet component and add totalAmount

--- src/pages/NFTWallet.js
+++ src/pages/NFTWallet.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Col from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Button';
 import '../styles/wallet.scss';
 import ownedNfts from '../components/exampleData';
 import placeholderImage from '../icons/placeholder.png';
+import NftNavBar from '../components/nftNavBar';
+
 
 class Wallet extends React.Component {
   constructor(props) {
@@ -12,7 +11,8 @@ class Wallet extends React.Component {
     this.state = {
       address: '',
       currency: '',
-      data: []
+      data: [],
+      totalAmount: 0
     };
   }
 
@@ -52,15 +52,19 @@ class Wallet extends React.Component {
       elements.push(<img key={index + 1} className="nft-image" src={placeholderImage} alt='NFT' />)
     }
 
-    const collection_logo_src = nft.collection.bannerImageUrl? nft.collection.bannerImageUrl : placeholderImage;
+    const collection_logo_src = nft.collection.bannerImageUrl ? nft.collection.bannerImageUrl : placeholderImage;
     const name = nft.name ? nft.name : nft.contract.name;
 
     console.log(name, collection_logo_src)
 
+    let total = nft.contract.openSeaMetadata.floorPrice ? (Math.round(nft.contract.openSeaMetadata.floorPrice * 10000000) / 10000000) : '0'
+
     elements.push(<p key={index + 2} className='nft-name'> {name}</p>)
     elements.push(<div key={index + 3} className='nft-footer'>
-      <img className='nft-collection-logo' src={collection_logo_src} alt='Collection Logo' />
-      <p key={index + 4} className='nft-floor-price'> {nft.contract.openSeaMetadata.floorPrice ? (Math.round(nft.contract.openSeaMetadata.floorPrice * 10000000) / 10000000) : '0'} ETH</p>
+      <a href={'https://opensea.io/collection/' + nft.collection.slug} target='_blank' rel='noreferrer noopener' className='nft-collection-name'>
+        <img className='nft-collection-logo' src={collection_logo_src} alt='Collection Logo' />
+      </a>
+      <p key={index + 4} className='nft-floor-price'> {total} ETH</p>
     </div>)
     return (
       // <p>hi</p>, <- Cant do this. Have to add extra data in the above statements
@@ -73,75 +77,28 @@ class Wallet extends React.Component {
     const { address, currency } = this.state;
     return (
       <div id='nft-wrapper'>
-        <div>
-          <h1 id='heading'>NFT Portfolio Tracker</h1>
-          {this.state.data.length === 0 ?
-
-            <div>
-              <h2 id='output'>Loading...</h2>
-            </div> :
-            <div>
-              <h2 id='output'>NFTs in {currency} wallet {address}</h2>
-              <div id='nft-container'>
-                {this.state.data.map((nft, index) => (
-                  <div key={index} className='nft-card'>
-                    {this.processImg(nft, index)}
-                  </div>
-                ))}
-              </div>
+        <h1 id='heading'>NFT Portfolio Tracker</h1>
+        <h2 id='output'>NFTs in {currency} wallet {address}</h2>
+        {this.state.data.length === 0 ?
+          <div>
+            <h2 id='output'>Loading...</h2>
+          </div> :
+          <div id="card-wrapper">
+            <h3 id='title'>NFTs</h3>
+            <NftNavBar />
+            <div id='nft-container'>
+              {this.state.data.map((nft, index) => (
+                <div key={index} className='nft-card'>
+                  {this.processImg(nft, index)}
+                </div>
+              ))}
             </div>
-          }
-        </div>
+
+          </div>
+        }
       </div>
     );
   }
 }
-
-
-
-// render() {
-//   const { address, currency, data } = this.state;
-
-//   // Grouping NFTs by collection
-//   const nftsByCollection = {};
-//   data.forEach(nft => {
-//     const collectionName = nft.collection.name;
-//     if (!nftsByCollection[collectionName]) {
-//       nftsByCollection[collectionName] = [];
-//     }
-//     nftsByCollection[collectionName].push(nft);
-//   });
-
-//   // Sorting collections alphabetically
-//   const sortedCollections = Object.keys(nftsByCollection).sort();
-
-//   return (
-//     <div id='nft-wrapper'>
-//       <div>
-//         <h1 id='heading'>NFT Portfolio Tracker</h1>
-//         {data.length === 0 ?
-//           <div>
-//             <h2 id='output'>Loading...</h2>
-//           </div> :
-//           <div>
-//             <h2 id='output'>NFTs in {currency} wallet {address}</h2>
-//             <div id='nft-container'>
-//               {sortedCollections.map(collectionName => (
-//                 <div key={collectionName}>
-//                   <h3>{collectionName}</h3>
-//                   {nftsByCollection[collectionName].map((nft, index) => (
-//                     <div key={index} className='nft-card'>
-//                       {this.processImg(nft)}
-//                     </div>
-//                   ))}
-//                 </div>
-//               ))}
-//             </div>
-//           </div>
-//         }
-//       </div>
-//     </div>
-//   );
-// }
 
 export default Wallet;
