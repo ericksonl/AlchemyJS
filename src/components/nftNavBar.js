3fd6720a289d18c14bcdbedd3650949688bdb8ce
Liam <liamerickson20@gmail.com>
2024-02-20 04:02:28 -0800

    Add nftNavBar component

---
+++ src/components/nftNavBar.js
@@ -0,0 +1,37 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+import React from 'react';
+import gatherCollections from './GetCollections';
+
+const nftNavBar = () => {
+    gatherCollections(); // This line is causing the error
+    return (
+        <div id='nft-nav-bar'>
+            <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic-button">
+                    All Collections
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic-button">
+                    Dropdown Button
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
+            <input className='search' />
+        </div>
+    )
+}
+
+export default nftNavBar;
