5221eaad8e7976589f6f12dae3110d8d064f2572
Liam <liamerickson20@gmail.com>
2024-02-20 15:42:19 -0800

    Refactor DataGather component to include dropdown menu for collections

--- src/components/DataGather.js
+++ src/components/DataGather.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ownedNfts from './exampleData';
-import NftNavBar from './nftNavBar';
+import NavItem from './NavItem';
 import NFTCard from '../components/NFTCard';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class DataGather extends React.Component {
   constructor(props) {
@@ -9,57 +10,60 @@ class DataGather extends React.Component {
     this.state = {
       address: this.props.address,
       currency: this.props.currency,
-      data: []
+      data: [],
+      collections: []
     };
   }
 
   componentDidMount() {
     this.setState({
-      data: ownedNfts
+      data: ownedNfts,
+      collections: ownedNfts.map((nft) => {
+        return nft.collection.name
+      })
     })
 
-    console.log(this.props.address)
-
-    // const { address } = this.props;
-    // let options = { method: 'GET', headers: { accept: 'application/json' } }
-    // fetch(`https://eth-mainnet.g.alchemy.com/nft/v3/${process.env.React_App_API_KEY}/getNFTsForOwner?owner=${address}&withMetadata=true&pageSize=100`, options)
-    //   .then(res => res.json())
-    //   .then((result) => {
-    //     console.log(result.ownedNfts)
-    //     this.setState({
-    //       data: result.ownedNfts
-    //     })
-    //   })
-    //   .catch(error => {
-    //     console.error('Error:', error)
-    //   })
+    console.log(this.state.collections)
   }
 
   render() {
     return (
       <>
-        {this.state.data.length === 0 ?
-          <div>
-            <h2 id='output'>Loading...</h2>
-          </div> :
-          <div id="card-wrapper">
-            <h3 id='title'>NFTs</h3>
-            {/*BUG: This line is causing the error*/}
-            <NftNavBar />
-            <div id='nft-container'>
-              {
-                this.state.data.map((nft, index) => (
-                  <div key={index} className='nft-card' collection={nft.collection.name}>
-                    <NFTCard
-                      nft={nft}
-                      index={index}
-                    />
-                  </div>
-                ))
-              }
-            </div>
+        {
+          this.state.data.length === 0 ?
+            <div>
+              <h2 id='output'>Loading...</h2>
+            </div> :
+            <div id="card-wrapper">
+              <h3 id='title'>NFTs</h3>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-basic-button">
+                  Dropdown Button
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                  {
+                    this.state.collections.map(item => (
+                      <NavItem
+                        collection={item}
+                      />
+                    ))
+                  }
+                </Dropdown.Menu>
+              </Dropdown>
+              <div id='nft-container'>
+                { //Create NFT Cards dynamically
+                  this.state.data.map((nft, index) => (
+                    <div key={index} className='nft-card' collection={nft.collection.name}>
+                      <NFTCard
+                        nft={nft}
+                        index={index}
+                      />
+                    </div>
+                  ))
+                }
+              </div>
 
-          </div>
+            </div >
         }
       </>
     );
